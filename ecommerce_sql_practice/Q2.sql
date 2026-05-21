c62eeb48113b74afe0c37df2bd3baa6e63a53f4d
opethesaint <52231356+opethesaint@users.noreply.github.com>
2026-05-21 15:00:50 +0100

    Delete ecommerce_sql_practice directory

--- ecommerce_sql_practice/Q2.sql
+++
@@ -1,9 +0,0 @@
--- 2.	Find the most popular product category.
-
-SELECT Products
-    Category,
-    COUNT(*) AS total_products
-FROM Products
-GROUP BY Category
-ORDER BY total_products DESC
-LIMIT 1;
--- ecommerce_sql_practice/Q3.sql
+++
@@ -1,9 +0,0 @@
--- 3.	Show monthly sales totals for 2024.
-
-SELECT 
-    MONTHNAME(OrderDate) AS month,
-    SUM(TotalAmount) AS monthly_sales
-FROM Orders
-WHERE YEAR(OrderDate) = 2024
-GROUP BY MONTH(OrderDate), MONTHNAME(OrderDate)
-ORDER BY MONTH(OrderDate);
